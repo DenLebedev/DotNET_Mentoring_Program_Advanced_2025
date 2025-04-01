f3eb4aedcd4b058f1fbef464541e4996f4a4d088
Den V. Lebedev <dvl@inbox.ru>
2025-04-01 17:53:59 +0300

    Update Controllers
    Update Controllers
    - Remove WeatherForecast basic functionality;
    - Add CartingService Controller

---
+++ CartingService/Controllers/CartingServiceController.cs
@@ -0,0 +1,28 @@
+﻿using CartingService.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CartingService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartingServiceController : ControllerBase
+    {
+        private readonly ILogger<CartingServiceController> _logger;
+
+        public CartingServiceController(ILogger<CartingServiceController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetCart")]
+        public Cart GetCartById(int id)
+        {
+            return new Cart
+            {
+                Id = id,
+                Items = new List<Item>()
+            };
+        }
+    }
+}
