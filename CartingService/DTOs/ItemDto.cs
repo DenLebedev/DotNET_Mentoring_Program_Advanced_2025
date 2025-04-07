8a136e9d7a1e102bd21525fd59b911e3a5d47a24
Den V. Lebedev <dvl@inbox.ru>
2025-04-07 11:54:24 +0300

    Update Model and DTO

---
+++ CartingService/DTOs/ItemDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CartingService.DTOs
+{
+    public class ItemDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageURL { get; set; }
+        public string ImageAltText { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
