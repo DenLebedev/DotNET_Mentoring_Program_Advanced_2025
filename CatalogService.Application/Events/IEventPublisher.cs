0a05ec4f6d493c317c0aa91064d86c7197161962
Den V. Lebedev <dvl@inbox.ru>
2025-04-28 17:06:55 +0300

    Refactoring the local version of CatalogService

---
+++ CatalogService.Application/Events/IEventPublisher.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace CatalogService.Application.Events
+{
+    public interface IEventPublisher
+    {
+        Task PublishAsync<T>(T message);
+    }
+}
---
+++ CatalogService.Infrastructure/Messaging/SqsPublisher.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using Microsoft.Extensions.Configuration;
+using CatalogService.Application.Events;
+
+namespace CatalogService.Infrastructure.Messaging;
+public class SqsPublisher : IEventPublisher
+{
+    private readonly IAmazonSQS _sqsClient;
+    private readonly string _queueUrl;
+
+    public SqsPublisher(IAmazonSQS sqsClient, IConfiguration configuration)
+    {
+        _sqsClient = sqsClient;
+        _queueUrl = configuration["AWS:CatalogItemUpdatesQueueUrl"];
+    }
+
+    public async Task PublishAsync<T>(T message)
+    {
+        var body = JsonSerializer.Serialize(message);
+        await _sqsClient.SendMessageAsync(_queueUrl, body);
+    }
+}
